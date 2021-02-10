b853577dff3723b90bb516b225f20a163a6faac4
Giuseppe <cartellagiuseppe@hotmail.it>
2021-02-10 15:17:16 +0100

    authentication

---
+++ dogx_a_smart_dispenser/lib/services/auth.dart
@@ -0,0 +1,14 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  //sign in anonim
+
+  //sign in with email & pwd.
+
+  //register with email & pwd
+
+  //sign out
+
+}
