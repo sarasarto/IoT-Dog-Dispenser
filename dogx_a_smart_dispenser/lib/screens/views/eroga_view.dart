103ba490c5bc3b34e897b79d9fdca0e3cf018007
sarasarto <48065967+sarasarto@users.noreply.github.com>
2021-02-19 21:42:24 +0100

    Fatto ErogaView con tendina, leggere i commenti

--- dogx_a_smart_dispenser/lib/screens/views/eroga_view.dart
+++ dogx_a_smart_dispenser/lib/screens/views/eroga_view.dart
@@ -17,25 +17,154 @@ class ErogaView extends StatefulWidget {
 }
 
 class _ErogaViewState extends State<ErogaView> {
+  /*List<DropdownMenuItem<Animal>> _dropDown;
+  Animal _selectedAnimal;
+  List<Animal> animals = this.animals;
+  @override
+  void initState() {
+    _dropDown = buildDropDown(this.animals);
+    _selectedAnimal = _dropDown[0].value;
+    super.initState();
+  }
+
+  onChangeDropdownItem(Animal selectedAnimal) {
+    setState(() {
+      _selectedAnimal = selectedAnimal;
+    });
+  }
+
+  List<DropdownMenuItem<Animal>> buildDropDown(List animals) {
+    List<DropdownMenuItem<Animal>> items = List();
+    for (Animal animal in animals) {
+      items.add(
+        DropdownMenuItem(
+          value: animal,
+          child: Text(animal.name),
+        ),
+      );
+    }
+    return items;
+  }*/
+
+  final _formKey = GlobalKey<FormState>();
+
+  String _currentQnt;
+  Animal _currentAnimal;
+  int _currentStrenght;
+
   @override
   Widget build(BuildContext context) {
     List<Animal> animals = widget.animals;
-    print('siamo qua');
-    print(animals);
-
+    //print('siamo qua');
+    //print(animals);
+    Dispenser dispenser = widget.dispenser;
+    print('*************prima-->' + dispenser.qtnRation.toString());
     return Scaffold(
-        backgroundColor: Colors.brown[50],
-        appBar: AppBar(
-          title: Text('Eroga dal dispenser'),
-          backgroundColor: Colors.brown[400],
-          elevation: 0.0,
+      backgroundColor: Colors.brown[50],
+      appBar: AppBar(
+        title: Text(dispenser.id),
+        backgroundColor: Colors.brown[400],
+        elevation: 0.0,
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Text(
+              'Eroga dal dispenser',
+              style: TextStyle(fontSize: 18.0),
+            ),
+            SizedBox(height: 40.0),
+            Text(
+              'Scegli il tuo animale',
+              style: TextStyle(fontSize: 18.0),
+            ),
+            SizedBox(height: 20.0),
+            //dropdown
+            DropdownButtonFormField(
+                value: _currentAnimal ?? animals[0],
+                items: animals.map((animal) {
+                  //print(animal.name);
+                  return DropdownMenuItem(
+                    value:
+                        animal, //questo è il corrente che devo mettere nel db
+                    child: Text(animal.name, style: TextStyle(fontSize: 18.0)),
+                  );
+                }).toList(),
+                onChanged: (Animal value) {
+                  setState(() {
+                    _currentAnimal = value;
+                  });
+                }),
+            SizedBox(height: 20.0),
+            TextFormField(
+              decoration: InputDecoration(hintText: 'Quantità da erogare'),
+              validator: (val) => val.isEmpty ? 'Quantità da erogare' : null,
+              onChanged: (val) => setState(() => _currentQnt = val),
+            ),
+            //button
+            RaisedButton(
+                color: Colors.pink[400],
+                child: Text('Eroga', style: TextStyle(color: Colors.white)),
+                onPressed: () async {
+                  print(_currentQnt);
+                  print(_currentAnimal.name);
+                  if (_currentAnimal.availableRation >=
+                      int.parse(_currentQnt)) {
+                    print('prima-->' + dispenser.qtnRation.toString());
+                    dispenser.setqtnRation(int.parse(_currentQnt));
+                    print('dopo-->' + dispenser.qtnRation.toString());
+                  } else {
+                    showDialog(
+                        context: context,
+                        barrierDismissible:
+                            false, // disables popup to close if tapped outside popup (need a button to close)
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: Text(
+                              "La quantità super la razione disponibile",
+                            ),
+                            content: Text("Sei sicuro?"),
+                            //buttons METTIAMO LA POSSIBILIà DI DARGLI PIU CIBO?????
+                            actions: <Widget>[
+                              FlatButton(
+                                child: Text("Close"),
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                }, //closes popup
+                              ),
+                            ],
+                          );
+                        });
+                  }
+                })
+          ],
         ),
-        body: /*Text(animals[0].name))*/
-            Container(
-          child: Column(
-            children: [for (var a in animals) Text(a.name)],
-          ),
-        )
-        );
+      ),
+    );
   }
 }
+
+/*child: Column(
+            children: [for (var a in animals) Text(a.name)],
+          ),*/
+/*child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("Select an animal"),
+              SizedBox(
+                height: 20.0,
+              ),
+              DropdownButton(
+                value: _selectedAnimal,
+                items: _dropDown,
+                onChanged: onChangeDropdownItem,
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Text('Selected: ${_selectedAnimal.name}'),
+            ],
+          ),
+        ),*/
