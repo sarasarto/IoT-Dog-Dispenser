bbeeedbf4ae962b2544029679acc00f2fced5c86
Giuseppe <cartellagiuseppe@hotmail.it>
2021-02-26 18:09:38 +0100

    implemented fsm

--- dispenserController/dispenserController.ino
+++ dispenserController/dispenserController.ino
@@ -16,16 +16,22 @@ int distance;
 
 Stepper stepper(nStep, pinOne, pinTwo, pinThree, pinFour);
 
+// STATES: 0:FAR, 1:NEAR
+int currentState;
+int futureState;
+
 void setup() {
   Serial.begin(9600);
   stepper.setSpeed(motor_speed);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
+  currentState = 0;
 }
 
 int command;
 
 void loop() {
+  futureState = 0;
   
   if (Serial.available()>0){
     command = Serial.read();
@@ -42,14 +48,24 @@ void loop() {
 
   duration = pulseIn(echoPin, HIGH);
   distance = duration*0.034/2;
+  //Serial.println(distance);
+
 
   delay(200);
 
+  if (currentState==0 && distance<15) futureState=1;
+  if (currentState==0 && distance>=15) futureState=0;
+  if (currentState==1 && distance>=15) futureState=0;
+  if (currentState==1 && distance<15) futureState=1;
 
-  if(distance < 10){
-    //invio al bridge la richiesta di controllo razione per quell'animale
+
+
+  //onEnter actions
+  if(futureState==1 && currentState==0){
     Serial.write(0xff);
     Serial.write(0x01);
     Serial.write(0xfe);
-  } 
+  }
+
+  currentState = futureState;
 }
